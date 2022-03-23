bec5f25e2b1ce7d1ce4bb25db1eb2783e9801479
boghiu adrian <boghiu.emilia88@gmail.com>
2022-03-23 12:08:18 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://emily:test@cluster0.7oibi.mongodb.net/api-menuiserie',
+      {
+        autoCreate: true,
+      },
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
